841019089d04a83a343117aae444bff8c468651d
Iradanov <iradanovi@tvz.hr>
2023-05-20 12:31:19 +0200

    Inicijalizacija Backenda

---
+++ QuizBackend/src/main/java/hr/tvz/project/quizbackend/controllers/UserController.java
@@ -0,0 +1,28 @@
+package hr.tvz.project.quizbackend.controllers;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping
+    public ResponseEntity<String> testResponse(){
+        return new ResponseEntity<>("OK", HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> postResponse(){
+        return new ResponseEntity<>("GotIt", HttpStatus.OK);
+    }
+    @PutMapping
+    public ResponseEntity<String> putResponse(){
+        return new ResponseEntity<>("Yes", HttpStatus.OK);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteResponse(@PathVariable Long id){
+        return new ResponseEntity<>("Exterminate: "+id, HttpStatus.OK);
+    }
+
+}
---
+++ QuizBackend/src/main/java/hr/tvz/project/quizbackend/domain/User.java
@@ -0,0 +1,4 @@
+package hr.tvz.project.quizbackend.domain;
+
+public class User {
+}
