9fb2c9d08b15cbe38aa244fa7c336ef0ca1b9cd4
shtef21 <stjepan.salopek@hotmail.com>
2023-06-06 14:14:22 +0200

    Revert "Revert "DB Setup (unfinished)""
    This reverts commit 8ee10351226155850a4c5ef3c957ee213c042457.

--- QuizBackend/src/main/java/hr/tvz/project/quizbackend/QuizBackendApplication.java
+++ QuizBackend/src/main/java/hr/tvz/project/quizbackend/QuizBackendApplication.java
@@ -2,6 +2,7 @@ package hr.tvz.project.quizbackend;
 
 import hr.tvz.project.quizbackend.logic.Database;
 import hr.tvz.project.quizbackend.repository.PlayerRepository;
+import hr.tvz.project.quizbackend.repository.QuizRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,14 +16,22 @@ public class QuizBackendApplication {
 	}
 
 	@Bean
-	public CommandLineRunner run(PlayerRepository playerRepository)
+	public CommandLineRunner run(
+			PlayerRepository playerRepository,
+			QuizRepository quizRepository)
 	{
 		return (args -> {
 
 			System.out.println("Generating players...");
-			Database.InitializePlayers(playerRepository);
+			Database.initializePlayers(playerRepository);
 			playerRepository.findAll().stream().forEach(player -> {
 				System.out.println("\t Generated " + player.toString());
+			});
+
+			System.out.println("Generating Quizzes..");
+			Database.initializeQuizes(quizRepository);
+			quizRepository.findAll().stream().forEach(quiz -> {
+				System.out.println("\t Generated " + quiz.toString());
 			});
 
 		});
