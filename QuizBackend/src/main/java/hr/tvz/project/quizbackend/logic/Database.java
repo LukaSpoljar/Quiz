6bb829b66de3c5f6371e62d9210aa20371e68b08
shtef21 <stjepan.salopek@hotmail.com>
2023-06-06 14:14:10 +0200

    Revert "DB Setup (unfinished)"
    This reverts commit d115609c39f0b938722e4359c6bb089c40d84e5a.

--- QuizBackend/src/main/java/hr/tvz/project/quizbackend/logic/Database.java
+++ QuizBackend/src/main/java/hr/tvz/project/quizbackend/logic/Database.java
@@ -1,31 +1,15 @@
 package hr.tvz.project.quizbackend.logic;
 
 import hr.tvz.project.quizbackend.entity.PlayerDB;
-import hr.tvz.project.quizbackend.entity.QuestionDB;
-import hr.tvz.project.quizbackend.entity.QuizDB;
 import hr.tvz.project.quizbackend.repository.PlayerRepository;
-import hr.tvz.project.quizbackend.repository.QuizRepository;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Database {
 
-    public static void initializePlayers(PlayerRepository playerRepository) {
+    public static void InitializePlayers(PlayerRepository playerRepository)
+    {
         playerRepository.save(new PlayerDB("štef", "lozinka"));
         playerRepository.save(new PlayerDB("luka", "lozinka"));
         playerRepository.save(new PlayerDB("ivan", "lozinka"));
-    }
-
-    public static void initializeQuizes(QuizRepository quizRepository) {
-
-        List<QuestionDB> quizQuestions = new ArrayList<>();
-//        quizQuestions.add(
-//            new QuestionDB("asd")
-//        );
-
-        QuizDB quiz = new QuizDB("My first quiz!", quizQuestions);
-        quizRepository.save(quiz);
     }
 
 }
